b5ef98ef0a2d457e40ef4f70cf7d8fe07c31c44b
Calvin Li <cli.calvinli@gmail.com>
2025-08-17 17:20:21 +0800

    Add fastapi and write a basic GET/POST echo server

---
+++ http_api/api.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from starlette.responses import FileResponse
+from starlette.templating import Jinja2Templates
+from starlette.requests import Request
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory=".")
+
+@app.get("/")
+def index():
+    return FileResponse("index.html")
+
+@app.post("/form") 
+async def form(request: Request):
+    async with request.form() as form:
+        print(form)
+
+    return templates.TemplateResponse(
+        "echo.html",
+        context={"comment": form['comment'], "request": request}
+    )
